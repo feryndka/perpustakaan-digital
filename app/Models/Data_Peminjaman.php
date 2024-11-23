b42eac223efaee5b8f4a23fb712b2d516e47190b
mardijker <33520837+mardijker@users.noreply.github.com>
2024-11-23 19:34:19 +0800

    Add Models for Data Peminjaman, add Session.php, add Dockerfile for NPM

---
+++ app/Models/Data_Peminjaman.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Data_Peminjaman extends Model
+{
+    protected $guarded =[];
+    protected $table = 'data_peminjaman';
+}
